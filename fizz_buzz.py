674439e6affe49d185aa80f6b88006edb2b32905
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-07 12:15:44 +0100

    Add functionality for only fizz and only buzz

---
+++ fizz_buzz.py
@@ -0,0 +1,5 @@
+def fizz_buzz(input_number):
+    if input_number % 3 == 0:
+        return "fizz"
+    elif input_number % 5 == 0:
+        return "buzz"
--- fizz_buzz_test.py
+++ fizz_buzz_test.py
@@ -6,3 +6,19 @@ class TestFizzBuzz(unittest.TestCase):
     # @unittest.skip("")
     def test_3_is_fizz(self):
         self.assertEqual("fizz", fizz_buzz(3))
+
+    def test_any_multiple_of_3_is_fizz(self):
+        self.assertEqual("fizz", fizz_buzz(6))
+        self.assertEqual("fizz", fizz_buzz(18))
+        self.assertEqual("fizz", fizz_buzz(12))
+        self.assertEqual("fizz", fizz_buzz(9))
+        self.assertNotEqual("fizz", fizz_buzz(8))
+    
+    def test_5_is_buzz(self):
+        self.assertEqual("buzz", fizz_buzz(5))
+    
+    def test_any_multiple_of_5_buzz(self):
+        self.assertEqual("buzz", fizz_buzz(10))
+        # self.assertEqual("buzz", fizz_buzz(15))
+        self.assertEqual("buzz", fizz_buzz(20))
+        self.assertEqual("buzz", fizz_buzz(25))
